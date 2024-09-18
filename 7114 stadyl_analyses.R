3a6b63fcb04ce7fe7f673da4ab5b9121e2c94c27
Meet261 <meetkumarmangroliya5@gmail.com>
2024-09-18 09:30:59 +0200

    Update 7114 stadyl_analyses.R

--- 7114 stadyl_analyses.R
+++ 7114 stadyl_analyses.R
@@ -1,3 +1,7 @@
+# Install reghelper from CRAN if not already installed
+if (!require("reghelper")) {
+    install.packages("reghelper", repos = "http://cran.us.r-project.org")
+}
 # Recode values for missing data (9, 999) in the whole dataset as NA
 data <- data %>% mutate_all(~na_if(., -999))
 data <- data %>% mutate_all(~na_if(., -9))
