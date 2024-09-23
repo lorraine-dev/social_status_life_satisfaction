e57ec3aa15554f937a37ef92084353d738ac08ab
Meet261 <meetkumarmangroliya5@gmail.com>
2024-09-23 11:13:40 +0200

    Update 7114 stadyl_analyses.R

--- 7114 stadyl_analyses.R
+++ 7114 stadyl_analyses.R
@@ -2,19 +2,11 @@
 if (!require("reghelper")) {
     install.packages("reghelper", repos = "http://cran.us.r-project.org")
 }
-# Define the URL of the dataset
-data_url <- "https://osf.io/r4wg2/download"
+# Source the data import script (which contains the data import function)
+source("data_import.R")
 
-# Define the file path to save the downloaded file locally
-file_path <- "StadyL_Study2.sav"
-
-# Download the dataset from OSF if it doesn't exist locally
-if (!file.exists(file_path)) {
-    download.file(data_url, destfile = file_path, mode = "wb")
-}
-
-# Load the dataset (SPSS format) into R
-data <- haven::read_sav(file_path)
+# Call the function to import and load the dataset
+data <- import_and_save_data()
 
 # Recode values for missing data (9, 999) in the whole dataset as NA
 data <- data %>% mutate_all(~na_if(., -999))
